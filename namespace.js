de848746ad150c39e91d2441730d469db175d3a0
Marcos <zorz0004@algonquinlive.com>
2018-10-19 19:56:02 -0400

    test4

--- namespace.js
+++ namespace.js
@@ -12,40 +12,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 
 var zorz0004 = function init() {
-    let newCircleSection = document.createElement("section");
-    newCircleSection.className = "circle";
-    newCircleSection.textContent = "A Circle";
-    // append the new element to the parent element with the id "circles"
-    let circles = document.getElementById("circles");
-    circles.appendChild(newCircleSection);
-   
-    // add some event listeners:
-
-    newCircleSection.addEventListener("click", circleClick);
-    newCircleSection.addEventListener("mouseover", mouseOver);
-    newCircleSection.addEventListener("mouseout", mouseOut);
-}
-
-
-function circleClick() {
-    this.style.borderColor = "blue";
-    this.style.backgroundColor = "aqua";
-    console.log(this);
-}
-
-function mouseOver(e) {
-    e.target.classList.toggle("focus");
-    console.log(e);
-    console.log(e.target);
-}
-
-function mouseOut(e) {
-    e.target.classList.toggle("focus");
-    e.target.removeAttribute('style'); // remove any style added by the click event
-    console.log(e);
-    console.log(e.target);
-}
-    /*
+    
     zorz0004 = document.querySelector("#zorz004");
     zorz0004.addEventListener("mouseover", mouseOver);
     
@@ -53,7 +20,7 @@ function mouseOut(e) {
 
 function mouseOver() {
             this.style.backgroundColor = "blue";
-    }*/
+    }
     
     
     
