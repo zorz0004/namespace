12c5491a219a12251dd172ac524e8003a6a88120
Marcos <zorz0004@algonquinlive.com>
2018-10-19 19:54:49 -0400

    test3

--- namespace.js
+++ namespace.js
@@ -8,13 +8,44 @@ Date: October 19, 2018
 
 
 /*---- Create variables with var  ----*/
-var zorz0004 = null;
-
 document.addEventListener("DOMContentLoaded", init);
 
 
-function init() {
-    
+var zorz0004 = function init() {
+    let newCircleSection = document.createElement("section");
+    newCircleSection.className = "circle";
+    newCircleSection.textContent = "A Circle";
+    // append the new element to the parent element with the id "circles"
+    let circles = document.getElementById("circles");
+    circles.appendChild(newCircleSection);
+   
+    // add some event listeners:
+
+    newCircleSection.addEventListener("click", circleClick);
+    newCircleSection.addEventListener("mouseover", mouseOver);
+    newCircleSection.addEventListener("mouseout", mouseOut);
+}
+
+
+function circleClick() {
+    this.style.borderColor = "blue";
+    this.style.backgroundColor = "aqua";
+    console.log(this);
+}
+
+function mouseOver(e) {
+    e.target.classList.toggle("focus");
+    console.log(e);
+    console.log(e.target);
+}
+
+function mouseOut(e) {
+    e.target.classList.toggle("focus");
+    e.target.removeAttribute('style'); // remove any style added by the click event
+    console.log(e);
+    console.log(e.target);
+}
+    /*
     zorz0004 = document.querySelector("#zorz004");
     zorz0004.addEventListener("mouseover", mouseOver);
     
@@ -22,7 +53,7 @@ function init() {
 
 function mouseOver() {
             this.style.backgroundColor = "blue";
-    }
+    }*/
     
     
     
