f187ff19e6a529ffa2c969c8287c3673eda4d42e
Marcos <zorz0004@algonquinlive.com>
2018-10-23 14:30:37 -0400

    test20

--- namespace.js
+++ namespace.js
@@ -10,37 +10,47 @@ Date: October 19, 2018
 //document.addEventListener("DOMContentLoaded", init);
 
 
-var ZORZ0004 = { init: function() {
+var ZORZ0004 = {
+    init: function () {
 
-    var e = document.createElement("div");
-    e.className = "box";
-    e.textContent = "zorz0004 \n SUCCESS";
-    /**********************/
-    e.style.padding = "1rem";
-    e.style.border =  "0.5rem outset gray";
-    e.style.backgroundColor = "white";
-    e.style.margin = "0 1rem 1rem";
-    e.style.width = "120px";
-    e.style.height = "120px";
-    e.style.float = "left";
-    e.style.boxSizing = "border-box";
-    /**********************/
-    
-    document.getElementById("boxes").appendChild(e);
+        var e = document.createElement("div");
+        e.className = "box";
+        e.textContent = "zorz0004 \n SUCCESS";
         
-    e.addEventListener("click", function () {
-        e.style.borderColor = "red";
-    });
-    
-    e.addEventListener("mouseover", function () {
-        e.style.backgroundColor = "blue";
-    });
-    
-    
-    e.addEventListener("mouseout", function () {
-        e.removeAttribute("style");
-    });
-}
-}
+        /**********************/
+        e.style.padding = "1rem";
+        e.style.border = "0.5rem outset gray";
+        e.style.backgroundColor = "white";
+        e.style.margin = "0 1rem 1rem";
+        e.style.width = "120px";
+        e.style.height = "120px";
+        e.style.float = "left";
+        e.style.boxSizing = "border-box";
+        /**********************/
+
+        document.getElementById("boxes").appendChild(e);
+
+        e.addEventListener("click", function () {
+            e.style.borderColor = "red";
+        });
+
+        e.addEventListener("mouseover", function () {
+            e.style.backgroundColor = "blue";
+        });
 
 
+        e.addEventListener("mouseout", function () {
+            e.removeAttribute("style");
+            /**********************/
+            e.style.padding = "1rem";
+            e.style.border = "0.5rem outset gray";
+            e.style.backgroundColor = "white";
+            e.style.margin = "0 1rem 1rem";
+            e.style.width = "120px";
+            e.style.height = "120px";
+            e.style.float = "left";
+            e.style.boxSizing = "border-box";
+            /**********************/
+        });
+    }
+}
