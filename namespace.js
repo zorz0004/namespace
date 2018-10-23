ab196d75285ab1358f9c32bdf14eeb2cfe201abf
Marcos <zorz0004@algonquinlive.com>
2018-10-23 14:25:45 -0400

    test18

--- namespace.js
+++ namespace.js
@@ -14,17 +14,17 @@ var ZORZ0004 = { init: function() {
 
     var e = document.createElement("div");
     e.className = "box";
-    e.textContent = "zorz0004 \n SUCESS";
-    
-//    e.style.padding = "1rem";
-//    e.style.border =  "0.5rem solid #d4d4d4";
-//    e.style.backgroundColor =  "#e9e9e9";
-//    e.style.margin = "0 1rem 1rem";
-//    e.style.width = "120px";
-//    e.style.height = "120px";
-//    e.style.float = "left";
-//    e.style.boxSizing = "border-box";
-    
+    e.textContent = "zorz0004 \n SUCESS \n :) ";
+    /**********************/
+    e.style.padding = "1rem";
+    e.style.border =  "0.5rem solid black";
+    e.style.backgroundColor = "white";
+    e.style.margin = "0 1rem 1rem";
+    e.style.width = "120px";
+    e.style.height = "120px";
+    e.style.float = "left";
+    e.style.boxSizing = "border-box";
+    /**********************/
     
     document.getElementById("boxes").appendChild(e);
         
